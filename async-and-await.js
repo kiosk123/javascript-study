ca35ad8313a0c1a0ba88205d988ceee99626e38d
Heo Jong Tae <heo9910@naver.com>
2021-03-03 00:45:18 +0900

    .

--- async-and-await.js
+++ async-and-await.js
@@ -11,18 +11,33 @@ async function process() {
     console.log('안녕하세요')
     await sleep(1000)
     console.log('반갑습니다.')
+    return true
 }
+
+// async 반환 타입은 promise기 때문에 then() 체이닝 가능
+process().then(console.log) // true 출력
 
 async function makeError() {
     await sleep(1000)
     throw new Error("make error")
 }
 
-// 비동기 함수 에러 처리
+// 비동기 함수 에러 catch 처리
 async function errorProcess() {
     try {
         await makeError()
     } catch (e) {
+        console.log("ERROR CAUGHT")
         console.log(e)
     }
 }
+
+errorProcess()
+
+// 비동기 함수에서 에러 throw
+async function throwError() {
+    await makeError()
+}
+
+// 비동기 함수에서 에러 throw시 catch 체이닝 가능
+throwError().catch(console.log)
