702462121927bb4d2e211261a20dd3080baaea95
Heo Jong Tae <heo9910@naver.com>
2021-03-03 00:23:01 +0900

    .

--- promise.js
+++ promise.js
@@ -20,3 +20,16 @@ increaseAndPrint(0)
 .then(increaseAndPrint)
 .then(increaseAndPrint)
 .catch(console.log)
+
+
+// 프로미스를 여러개 한번에 실행
+const promise1 = Promise.resolve('성공1') //resolve Promise 생성
+const promise2 = Promise.resolve('성공2')
+
+Promise.all([promise1, promise2])
+ .then((result) => {
+     console.log(result) // ['성공1', '성공2']
+ })
+ .catch((error) => {
+     console.error(error)
+ })
